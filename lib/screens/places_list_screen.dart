0452d0abb790f8862e4863253c8243e245e4ddb9
seokhyeon12345@gmail.com <seokhyeon12345@gmail.com>
2022-07-02 23:13:53 +0900

    FEAT : add add place feature with take picture add take picture on add place screen add list of added place

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import './add_place_screen.dart';
+import '../providers/great_places.dart';
 
 class PlacesListscreen extends StatelessWidget {
   const PlacesListscreen({Key? key}) : super(key: key);
@@ -9,18 +11,34 @@ class PlacesListscreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Your places'),
+        title: const Text('Your places'),
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.add),
+            icon: const Icon(Icons.add),
             onPressed: () {
               Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
             },
           ),
         ],
       ),
-      body: Center(
-        child: CircularProgressIndicator(),
+      body: Consumer<GreatPlaces>(
+        child: const Center(
+          child: Text('Got no places yet, start adding some!'),
+        ),
+        builder: (context, greatPlaces, child) => greatPlaces.items.isEmpty
+            ? child!
+            : ListView.builder(
+                itemCount: greatPlaces.items.length,
+                itemBuilder: (ctx, i) => ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: FileImage(
+                      greatPlaces.items[i].image,
+                    ),
+                  ),
+                  title: Text(greatPlaces.items[i].title),
+                  onTap: () {},
+                ),
+              ),
       ),
     );
   }
--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -2,9 +2,13 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspath;
 
 class ImageInput extends StatefulWidget {
-  const ImageInput({Key? key}) : super(key: key);
+  final Function onSelectImage;
+
+  const ImageInput(this.onSelectImage, {Key? key}) : super(key: key);
 
   @override
   State<ImageInput> createState() => _ImageInputState();
@@ -14,10 +18,20 @@ class _ImageInputState extends State<ImageInput> {
   File? _storedImage;
 
   Future<void> _takePicutre() async {
-    final imageFile = await ImagePicker().pickImage(
+    final picker = ImagePicker();
+    final imageFile = await picker.pickImage(
       source: ImageSource.camera,
       maxWidth: 600,
     );
+    if (imageFile != null) {
+      setState(() {
+        _storedImage = File(imageFile.path);
+      });
+      final appDir = await syspath.getApplicationDocumentsDirectory();
+      final fileName = path.basename(imageFile.path);
+      final saveImage = await _storedImage!.copy('${appDir.path}/$fileName');
+      widget.onSelectImage(saveImage);
+    }
   }
 
   @override

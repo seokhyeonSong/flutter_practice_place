6a3eaa0c49ee473d4ef441d1631651a28d3c02b9
seokhyeon12345@gmail.com <seokhyeon12345@gmail.com>
2022-07-03 01:02:32 +0900

    FEAT : save place data to SQLite DB when add place, then it saves to SQLite so that data not to be volatile

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -21,24 +21,34 @@ class PlacesListscreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Consumer<GreatPlaces>(
-        child: const Center(
-          child: Text('Got no places yet, start adding some!'),
-        ),
-        builder: (context, greatPlaces, child) => greatPlaces.items.isEmpty
-            ? child!
-            : ListView.builder(
-                itemCount: greatPlaces.items.length,
-                itemBuilder: (ctx, i) => ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: FileImage(
-                      greatPlaces.items[i].image,
+      body: FutureBuilder(
+        future: Provider.of<GreatPlaces>(context, listen: false)
+            .fetchAndSetPlaces(),
+        builder: (context, snapshot) =>
+            snapshot.connectionState == ConnectionState.waiting
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : Consumer<GreatPlaces>(
+                    child: const Center(
+                      child: Text('Got no places yet, start adding some!'),
                     ),
+                    builder: (context, greatPlaces, child) =>
+                        greatPlaces.items.isEmpty
+                            ? child!
+                            : ListView.builder(
+                                itemCount: greatPlaces.items.length,
+                                itemBuilder: (ctx, i) => ListTile(
+                                  leading: CircleAvatar(
+                                    backgroundImage: FileImage(
+                                      greatPlaces.items[i].image,
+                                    ),
+                                  ),
+                                  title: Text(greatPlaces.items[i].title),
+                                  onTap: () {},
+                                ),
+                              ),
                   ),
-                  title: Text(greatPlaces.items[i].title),
-                  onTap: () {},
-                ),
-              ),
       ),
     );
   }
